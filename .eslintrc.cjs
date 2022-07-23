33f0b67f5b1a6f82dc23b14bfc3f6f28255c589a
sakait-pc <mhdksamd@gmail.com>
2022-07-23 16:24:03 +0900

    build: setup eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": ["./tsconfig.json"]
+    },
+    "plugins": [
+        "react",
+        "react-hooks",
+        "@typescript-eslint"
+    ],
+    "ignorePatterns": [".eslintrc.cjs"],
+    "rules": {
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-filename-extension": [1, {extensions: [".js", ".jsx", ".tsx"]}],
+        "no-use-before-define": ["error", {variables: false}],
+        "react/prop-types": [0],
+        "@typescript-eslint/no-explicit-any": 0,
+        "@typescript-eslint/no-inferrable-types": 0,
+        "@typescript-eslint/no-use-before-define": 0
+    }
+}
